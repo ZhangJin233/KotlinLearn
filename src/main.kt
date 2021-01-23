73d07a38627dcd62a877e11c2f4282aab314b27a
ZhangJin233 <jane.zhangjin@outlook.com>
2021-01-23 19:57:42 +0800

    Add main file

---
+++ src/main.kt
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Jane
+ * @date 2021-01-23 7:43 PM
+ *
+ */
+
+val name = "jane"
+var greeting:String? = null
+fun main(){
+
+    println(name)
+    println(greeting)
+    greeting = "hi"
+
+    println(name)
+    println(greeting)
+}
